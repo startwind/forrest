05bf0ebe6fa603b0f1fbfa278edc1ed438f210b6
Nils Langner <nils.langner@phmlabs.com>
2023-05-01 20:22:37 +0200

    unused use-statement

--- src/CliCommand/Directory/RemoveCommand.php
+++ src/CliCommand/Directory/RemoveCommand.php
@@ -7,7 +7,6 @@ use Symfony\Component\Console\Command\Command as SymfonyCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Yaml\Yaml;
 
 class RemoveCommand extends DirectoryCommand
 {
