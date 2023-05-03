cfb9f89dbe424868a978d2d98a5c085661804926
Nils Langner <nils.langner@phmlabs.com>
2023-05-03 17:30:20 +0200

    downgrade

--- rector.php
+++ rector.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
