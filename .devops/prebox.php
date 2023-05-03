f3f1f9849273a11da701f058a364557197f99200
Nils Langner <nils.langner@phmlabs.com>
2023-05-03 17:24:13 +0200

    fixed filename

--- .devops/prebox.php
+++ .devops/prebox.php
@@ -1,3 +1,3 @@
 <?php
 
-file_put_contents("phar://bin/forrest.phar/vendor/composer/platform_check.php to/phar.phar/entry.php", '');
+file_put_contents("phar://bin/forrest.phar/vendor/composer/platform_check.php", '');
