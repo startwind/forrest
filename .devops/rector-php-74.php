48968392177ce393e7f4945659f2db22d703bc46
Nils Langner <nils.langner@leankoala.com>
2023-05-03 18:40:06 +0200

    PHAR compatible with PHP 7.4 (#46)
    * added rector
    
    * no check
    
    * change PHP req in lock file
    
    * disable aout check coposer
    
    * fixed replace
    
    * added src dir
    
    * ignore php
    
    * changed include path
    
    * better regex
    
    * ex vs in
    
    * no include
    
    * empty file
    
    * list
    
    * better ls
    
    * ignore all reqs
    
    * later ls
    
    * better box config
    
    * prebox
    
    * fixed filename
    
    * downgrade
    
    * added bin dir
    
    * move rector file
    
    * namespaced
    
    * new application name
    
    * new docs

---
+++ .devops/rector-php-74.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\DowngradeLevelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/vendor',
+        __DIR__ . '/bin'
+    ]);
+
+    $rectorConfig->sets([
+        DowngradeLevelSetList::DOWN_TO_PHP_74
+    ]);
+};
