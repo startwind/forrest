6bb7ee60e9aad21887580969d6c0c87ee5472216
Sebastian Neubert <sebastian.neubert@posteo.de>
2023-05-03 14:57:58 +0200

    feat: improve yaml file test

--- tests/Adapter/YamlAdapterTest.php
+++ tests/Adapter/YamlAdapterTest.php
@@ -11,12 +11,29 @@ final class YamlAdapterTest extends TestCase
     
     public function setup(): void 
     {
-        $this->subject = new YamlAdapter('username', 'prefix');
+        $this->subject = new YamlAdapter('yamlfile.yaml');
     }
 
     public function testGetType(): void
     {
         $this->assertEquals('yaml' , $this->subject->getType());
+    }
+
+    /**
+     * @dataProvider yamlConfigProvider
+     */
+    public function testConfigArray(array $config): void 
+    {
+        $result = YamlAdapter::fromConfigArray($config);
+        $this->assertInstanceOf(YamlAdapter::class, $result);
+    }
+
+    static public function yamlConfigProvider(): array
+    {
+        return [
+            [['file' => 'yaml.file']],
+            [['file' => 'yaml.file', 'somethingElse' => 'doesnt matter']],
+        ];
     }
 }
 
