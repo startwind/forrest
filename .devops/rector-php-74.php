88eb531c4fb8a65f14e6bb97382160a96597e754
Nils Langner <nils.langner@phmlabs.com>
2023-05-03 18:47:55 +0200

    update rector paths

--- .devops/rector-php-74.php
+++ .devops/rector-php-74.php
@@ -7,9 +7,9 @@ use Rector\Set\ValueObject\DowngradeLevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/src',
-        __DIR__ . '/vendor',
-        __DIR__ . '/bin'
+        __DIR__ . '/../src',
+        __DIR__ . '/../vendor',
+        __DIR__ . '/../bin'
     ]);
 
     $rectorConfig->sets([
