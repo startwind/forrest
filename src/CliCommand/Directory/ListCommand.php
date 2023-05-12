592df40a9944415ffb9e7946dff554a07b7bff11
Nils Langner <nils.langner@leankoala.com>
2023-05-12 07:57:54 +0200

    Official repos (#100)
    * official repos
    
    * sort order fix
    
    * pattern is now case in-senstive
    
    * implement --all flag

--- src/CliCommand/Directory/ListCommand.php
+++ src/CliCommand/Directory/ListCommand.php
@@ -5,12 +5,20 @@ namespace Startwind\Forrest\CliCommand\Directory;
 use Startwind\Forrest\Output\OutputHelper;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ListCommand extends DirectoryCommand
 {
     protected static $defaultName = 'directory:list';
     protected static $defaultDescription = 'List all repositories in the official Forrest directory.';
+
+    protected function configure()
+    {
+        // @todo as long as there are only a few repositories listed we show all. This mechanism should be activated
+        //       as soon as there a longer list.
+        $this->addOption('all', '', InputOption::VALUE_OPTIONAL, 'List all repositories. Default is that only official repositories are shown.', true);
+    }
 
     protected function doExecute(InputInterface $input, OutputInterface $output): int
     {
@@ -19,25 +27,60 @@ class ListCommand extends DirectoryCommand
 
         $directory = $this->getDirectory();
 
+        $repositories = $directory['repositories'];
+        ksort($repositories);
+
         $rows = [];
 
-        foreach ($directory['repositories'] as $identifier => $repository) {
-            $row = [
-                $identifier,
-                $repository['name'],
-                $repository['description'],
-            ];
+        $all = $input->getOption('all') !== false;
 
-            if (in_array($identifier, $activeRepositories)) {
-                $row[] = 'x';
+        $unofficialCount = 0;
+
+        foreach ($repositories as $identifier => $repository) {
+
+            if ($all || (array_key_exists('official', $repository) && $repository['official'] === true)) {
+                $row = [
+                    $identifier,
+                    $repository['name'],
+                    $repository['description'],
+                ];
+
+                if (in_array($identifier, $activeRepositories)) {
+                    $row[] = 'x';
+                } else {
+                    $row[] = '';
+                }
+
+                if ($all) {
+                    if (array_key_exists('official', $repository) && $repository['official'] === true) {
+                        $row[] = 'x';
+                    } else {
+                        $row[] = '';
+                    }
+                }
+
+                $rows [] = $row;
             } else {
-                $row[] = '';
+                $unofficialCount++;
             }
-
-            $rows [] = $row;
         }
 
-        OutputHelper::renderTable($output, ['Identifier', 'Name', 'Description', 'Installed'], $rows);
+        $headers = ['Identifier', 'Name', 'Description', 'Installed'];
+
+        if ($all) {
+            $headers[] = 'Official';
+        }
+
+        OutputHelper::renderTable($output, $headers, $rows);
+
+        if ($unofficialCount > 0) {
+            $output->writeln([
+                    '',
+                    'This list only contains official repositories. If you also want to see',
+                    'the other ' . $unofficialCount . ' unofficial repositories please use the --all option.'
+                ]
+            );
+        }
 
         return SymfonyCommand::SUCCESS;
     }
