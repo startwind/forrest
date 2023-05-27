ec82f853391a897d7cce3dbbdfe1e8679c9979a4
Nils Langner <nils.langner@phmlabs.com>
2023-05-27 14:05:11 +0200

    more php docs

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -4,9 +4,18 @@ namespace Startwind\Forrest\Logger;
 
 interface Logger
 {
+    /**
+     * Log an error.
+     */
     public function error($message): void;
 
+    /**
+     * Log a warning.
+     */
     public function warn($message): void;
 
+    /**
+     * Log an information.
+     */
     public function info($message): void;
 }
